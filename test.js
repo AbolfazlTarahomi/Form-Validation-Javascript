500afdf5bb50fc6fa931da0c35a01a58a1234b6e
AbolfazlTarahomi <abtarahomii@gmail.com>
2026-08-21 05:54:02 +0330

    on & off checkBox

--- test.js
+++ test.js
@@ -1,18 +1,22 @@
 const bio = document.getElementById('bio-input');
-const previewbio = document.querySelector(".preview  p");
-const bioinput = document.getElementById("bio-input");
+const previewbio = document.querySelector('.preview  p');
+const terms = document.getElementById('terms');
+const submitBtn = document.getElementById('submit-btn');
 
-
+// code
 
 bio.addEventListener('input', function (event) {
+  if (event.target.value) {
+    previewbio.innerHTML = event.target.value;
+  } else {
+    previewbio.innerHTML = 'please insert somthing.';
+  }
+});
 
-    if(event.target.value){
-        previewbio.innerHTML = event.target.value;
-    }
-    else
-    {
-         previewbio.innerHTML = "please insert somthing."
-    }
-
-
+terms.addEventListener('change', function (event) {
+  if (event.target.checked) {
+    submitBtn.disabled = false;
+  } else {
+    submitBtn.disabled = true;
+  }
 });
